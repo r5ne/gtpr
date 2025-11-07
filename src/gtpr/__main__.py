9a12ca2cb55710bbe86196319c10984e5a42512e
r5ne <renetimantsev@gmail.com>
2025-11-07 14:12:32 +0000

    cleaned up input_quit, leading to simpler logic. improved error handling and implemented reading files back into team objects

--- src/gtpr/__main__.py
+++ src/gtpr/__main__.py
@@ -1,34 +1,70 @@
 import pathlib
+import sys
 from typing import Literal
 
-import team
 import data
+import team
 
-DEFAULT_PATH = pathlib.Path.cwd() / "teams"
-
-
-def make_team() -> team.Team | Literal[False]:
-    name = input_quit("name: ")
-    if not name:
-        return False
-    return team.Team(name=name)
+TEAM_PATH = pathlib.Path.cwd() / "teams"
+EXIT_VALUE = "\\q"
 
 
-def input_quit(txt: str) -> str | Literal[False]:
+def input_quit(txt: str) -> str:
     output = input(txt)
-    if output == "\\q":
-        return False
+    if output == EXIT_VALUE:
+        sys.exit()
     return output
 
 
+def team_factory() -> team.Team:
+    name = input_quit("name: ")
+    return team.Team(name=name)
+
+
+def get_mode() -> Literal["w", "r"]:
+    while (
+        answer := input_quit('Write or read team? "w" for write and "r" for read.')
+    ) not in (
+        "w",
+        "r",
+    ):
+        print("Invalid mode selected, please try again.")
+    return answer
+
+
+def make_team() -> None:
+    while True:
+        new_team = team_factory()
+        try:
+            data.write_team(new_team, TEAM_PATH / f"{new_team.name}.json")
+        except FileExistsError:
+            print("Team already exists! Try another team name, or read the team.")
+        else:
+            return
+
+
+def load_team() -> team.Team:
+    while True:
+        team_name = input_quit("Enter the name of your team to load: ")
+        team_path = TEAM_PATH / f"{team_name}.json"
+        try:
+            loaded_team = data.read_team(team_path)
+        except FileNotFoundError:
+            print(f"Team not found at path: {team_path}. Try a different team name")
+        else:
+            return loaded_team
+
+
 def main() -> None:
-    running = True
-    print("gtpr by r5ne!\nat any point type \\q to quit safely")
-    while running:
-        if not (team := make_team()):
-            running = False
-        elif not data.write_team(team, DEFAULT_PATH / f"{team.name}.json"):
-            print("Team already exists!")
+    print(f"gtpr by r5ne!\nat any point type {EXIT_VALUE} to quit safely")
+    while True:
+        mode = get_mode()
+        match mode:
+            case "w":
+                make_team()
+            case "r":
+                loaded_team = load_team()
+                print(loaded_team.__str__())
 
 
 if __name__ == "__main__":
--- src/gtpr/data.py
+++ src/gtpr/data.py
@@ -1,18 +1,27 @@
 import json
 from typing import TYPE_CHECKING
 
+import team
+
 if TYPE_CHECKING:
     import pathlib
 
     from team import Team
 
 
-def write_team(team: Team, path: pathlib.Path, *, override: bool = False) -> bool:
+def write_team(
+    team: Team,
+    path: pathlib.Path,
+    *,
+    override: bool = False,
+) -> None:
     path.parent.mkdir(exist_ok=True)
-    try:
-        path.touch(exist_ok=override)
-    except FileExistsError:
-        return False
+    path.touch(exist_ok=override)
     with path.open("w") as jsonfile:
-        json.dump(team.model_dump(), jsonfile)
-    return True
+        json.dump(team.model_dump_json(), jsonfile)
+
+
+def read_team(path: pathlib.Path) -> team.Team:
+    with path.open() as jsonfile:
+        jsondata = json.load(jsonfile)
+        return team.Team.model_validate_json(jsondata)
