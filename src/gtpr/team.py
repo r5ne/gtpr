e70a06fcabc1dfabf782d05d0c930bbed831455c
r5ne <renetimantsev@gmail.com>
2025-11-14 13:30:56 +0000

    Added functions to calculate the dps differences between best and worst case per character scenarios and a way to then calculate artifact importance based on the teams overall dps.

--- src/gtpr/team.py
+++ src/gtpr/team.py
@@ -16,6 +16,7 @@ class Build(pydantic.BaseModel):
 
 class Character(pydantic.BaseModel):
     id: str = pydantic.Field(default_factory=lambda: uuid.uuid4().hex)
+
     name: str
     best_artifact_sets: list[str]
     artifact_set: str
@@ -52,10 +53,34 @@ class Team(pydantic.BaseModel):
     active_team_build_id: str | None = None
 
 
+def calculate_dps_diffs(team: Team, team_build: TeamBuild) -> None:
+    for character in team.characters:
+        team_build.character_no_substat_dps_diff[character.id] = (
+            team_build.team_dps - team_build.character_no_substat_dps[character.id]
         )
+        team_build.character_no_substat_optimal_dps_diff[character.id] = (
+            team_build.character_optimal_artifact_dps[character.id]
+            - team_build.character_no_substat_dps[character.id]
         )
+    team_build.team_no_substat_optimal_dps_diff = sum(
+        team_build.character_no_substat_optimal_dps_diff.values()
+    )
 
 
+def calculate_substat_importance(team: Team, team_build: TeamBuild) -> None:
+    for character in team.characters:
+        team_build.absolute_character_substat_power[character.id] = (
+            team_build.character_no_substat_dps_diff[character.id]
+            / team_build.character_no_substat_optimal_dps_diff[character.id]
+        )
+        team_build.relative_character_substat_power[character.id] = (
+            team_build.character_no_substat_dps_diff[character.id]
+            / team_build.team_no_substat_optimal_dps_diff
+        )
+        team_build.character_substat_importance[character.id] = (
+            team_build.character_no_substat_optimal_dps_diff[character.id]
+            / team_build.team_no_substat_optimal_dps_diff
+        )
 
 
 def new_team_name() -> str:
