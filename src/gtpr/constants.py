4c30b9a0116b8d0e57b47be0ae70d792720a8f2e
r5ne <renetimantsev@gmail.com>
2026-09-03 20:07:42 +0100

    Various changes, but working app. general commit before app overhaul.

--- src/gtpr/constants.py
+++ src/gtpr/constants.py
@@ -5,3 +5,13 @@ TEAM_PATH = pathlib.Path.cwd() / TEAM_FOLDER_NAME
 EXIT_VALUE = "\\q"
 
 SKILL = ("nope", "barely", "playable", "good", "mastered")
+SUBSTAT_VALUE = {
+    0.0: "just enough energy",
+    0.2: "weak",
+    0.4: "mid",
+    0.5: "aight",
+    0.6: "good",
+    0.7: "great",
+    0.8: "insane",
+    0.9: "perfect",
+}
--- src/gtpr/optimisertarget.py
+++ src/gtpr/optimisertarget.py
@@ -1,3 +1,6 @@
+import custominput
+
+
 def calculate_personal_dps_importance(
     original_dps: int,
     no_substat_dps: int,
@@ -7,3 +10,18 @@ def calculate_personal_dps_importance(
     dps_difference = original_dps - no_substat_dps
     personal_dps_difference = original_personal_dps - no_substat_personal_dps
     return personal_dps_difference / dps_difference
+
+
+if __name__ == "__main__":
+    original_dps = input("originaldps")
+    no_sub_dps = input("no_sub_dps")
+    original_personal_dps = input("originalmydps")
+    no_sub_personal_dps = input("nosubpersonaldps")
+    print(
+        calculate_personal_dps_importance(
+            original_dps=int(original_dps),
+            no_substat_dps=int(no_sub_dps),
+            original_personal_dps=int(original_personal_dps),
+            no_substat_personal_dps=int(no_sub_personal_dps),
+        )
+    )
