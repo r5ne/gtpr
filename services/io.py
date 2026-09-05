dd906533fa78b12539f3017cb4e1f4be7d6ef265
r5ne <renetimantsev@gmail.com>
2026-09-05 19:32:01 +0100

    Added team reading and deleting.

--- services/io.py
+++ services/io.py
@@ -20,3 +20,17 @@ def write_team(team: Team) -> None:
     team_file_path = TEAM_PATH / f"{team.name.lower().replace(" ", "-")}.json"
     with team_file_path.open("w") as json_file:
         json_file.write(team.model_dump_json(indent=4))
+
+def read_teams() -> list[Team]:
+    create_directories()
+
+    teams = []
+    for file in TEAM_PATH.glob("*.json"):
+        with file.open("r") as json_file:
+            teams.append(Team.model_validate_json(json_file.read()))
+    return teams
+
+def delete_team(team_name: str) -> None:
+    team_file_path = TEAM_PATH / f"{team_name.lower().replace(" ", "-")}.json"
+    if team_file_path.exists():
+        team_file_path.unlink()
