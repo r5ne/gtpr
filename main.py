ef5f8864141723905a74832e09855477d5ebd066
r5ne <renetimantsev@gmail.com>
2026-09-05 19:36:17 +0100

    Replaced home and team creation screen with a page layout. Improved home screen layout and allowed for viewing and deleting created teams.

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ st.set_page_config(page_title="GTPR", layout="wide")
 
 if "teams" not in st.session_state:
     st.session_state.teams = io.read_teams()
+
+pages = {
+    "Teams": [
+        st.Page("pages/1_home.py", title="Dashboard"),
+        st.Page("pages/2_team_builder.py", title="Team Builder"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
